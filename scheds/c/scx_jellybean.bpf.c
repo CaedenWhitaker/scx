f590ecabddbb533f2a4ae829309506cb1023ab76
CaedenWhitaker <caeden.whitaker@gmail.com>
2025-04-16 08:13:47 +0000

    jellybean: prototype

---
+++ scheds/c/scx_jellybean.bpf.c
@@ -0,0 +1,150 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2025 University of Wisconsin-Madison.
+ * Copyright (c) 2022 Meta Platforms, Inc. and affiliates.
+ * Copyright (c) 2022 Tejun Heo <tj@kernel.org>
+ * Copyright (c) 2022 David Vernet <dvernet@meta.com>
+ */
+#include <scx/common.bpf.h>
+
+#define SCHED_BATCH 3
+
+char _license[] SEC("license") = "GPL";
+
+const volatile bool fifo_sched;
+volatile bool throttled;
+volatile s64 batches;
+
+static u64 vtime_now;
+static u64 cpu_limit;
+UEI_DEFINE(uei);
+
+#define SHARED_DSQ 0
+#define BATCH_DSQ 1
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(key_size, sizeof(u32));
+	__uint(value_size, sizeof(u64));
+	__uint(max_entries, 4);
+} stats SEC(".maps");
+
+static void stat_inc(u32 idx)
+{
+	u64 *cnt_p = bpf_map_lookup_elem(&stats, &idx);
+	if (cnt_p)
+		(*cnt_p)++;
+}
+
+static void stat_set(u32 idx, u32 val)
+{
+	u64 *cnt_p = bpf_map_lookup_elem(&stats, &idx);
+	if (cnt_p)
+		(*cnt_p) = val;
+}
+
+static u32 stat_get(u32 idx)
+{
+	u64 *cnt_p = bpf_map_lookup_elem(&stats, &idx);
+	return cnt_p ? *cnt_p : 0;
+}
+
+s32 BPF_STRUCT_OPS(jellybean_select_cpu, struct task_struct *p, s32 prev_cpu, u64 wake_flags)
+{
+	bool is_idle = false;
+	s32 cpu;
+	cpu = scx_bpf_select_cpu_dfl(p, prev_cpu, wake_flags, &is_idle);
+	if (is_idle) {
+		stat_inc(0);
+		if(p->policy == SCHED_BATCH){
+			if(throttled){
+				return 0;
+			}
+			stat_inc(2);
+		}
+		scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL, SCX_SLICE_DFL, 0);
+	}
+	return cpu;
+}
+
+void BPF_STRUCT_OPS(jellybean_tick, struct task_struct *p)
+{
+	if (p->policy == SCHED_BATCH && throttled) {
+		s32 cpu = scx_bpf_get_cpu(p);
+		// kick the task out of the CPU
+		scx_bpf_kick_cpu(p, SCX_KICK_PREEMPT);
+	}
+}
+
+void BPF_STRUCT_OPS(jellybean_enqueue, struct task_struct *p, u64 enq_flags)
+{
+	stat_inc(1);
+	if(p->policy == SCHED_BATCH) stat_inc(2);
+	if (fifo_sched) {
+		scx_bpf_dsq_insert(p, SHARED_DSQ, SCX_SLICE_DFL, enq_flags);
+	} else {
+		u64 vtime = p->scx.dsq_vtime;
+		if (time_before(vtime, vtime_now - SCX_SLICE_DFL))
+			vtime = vtime_now - SCX_SLICE_DFL;
+
+		scx_bpf_dsq_insert_vtime(p, SHARED_DSQ, SCX_SLICE_DFL, vtime,
+					 enq_flags);
+	}
+}
+
+void BPF_STRUCT_OPS(jellybean_dispatch, s32 cpu, struct task_struct *prev)
+{
+	scx_bpf_dsq_move_to_local(SHARED_DSQ);
+}
+
+void BPF_STRUCT_OPS(jellybean_running, struct task_struct *p)
+{
+	if(p->policy == SCHED_BATCH){
+		stat_set(3, 1);
+	}
+	if (fifo_sched)
+		return;
+	if (time_before(vtime_now, p->scx.dsq_vtime))
+		vtime_now = p->scx.dsq_vtime;
+}
+
+void BPF_STRUCT_OPS(jellybean_stopping, struct task_struct *p, bool runnable)
+{
+	if(p->policy == SCHED_BATCH){
+		stat_set(3, 1);
+	}
+	if (fifo_sched)
+		return;
+
+	p->scx.dsq_vtime += (SCX_SLICE_DFL - p->scx.slice) * 100 / p->scx.weight;
+}
+
+void BPF_STRUCT_OPS(jellybean_enable, struct task_struct *p)
+{
+	p->scx.dsq_vtime = vtime_now;
+}
+
+s32 BPF_STRUCT_OPS_SLEEPABLE(jellybean_init)
+{
+	s32 ret;
+	if ((ret = scx_bpf_create_dsq(SHARED_DSQ, -1)) < 0) return ret;
+	if ((ret = scx_bpf_create_dsq(BATCH_DSQ, -1)) < 0) return ret;
+	cpu_limit = scx_bpf_get_cpu_limit();
+	return ret;
+}
+
+void BPF_STRUCT_OPS(jellybean_exit, struct scx_exit_info *ei)
+{
+	UEI_RECORD(uei, ei);
+}
+
+SCX_OPS_DEFINE(jellybean_ops,
+	       .select_cpu		= (void *)jellybean_select_cpu,
+		   .tick    		= (void *)jellybean_tick,
+	       .enqueue			= (void *)jellybean_enqueue,
+	       .dispatch		= (void *)jellybean_dispatch,
+	       .running			= (void *)jellybean_running,
+	       .stopping		= (void *)jellybean_stopping,
+	       .enable			= (void *)jellybean_enable,
+	       .init			= (void *)jellybean_init,
+	       .exit			= (void *)jellybean_exit,
+	       .name			= "jellybean");
---
+++ scheds/c/scx_jellybean.c
@@ -0,0 +1,158 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2025 University of Wisconsin-Madison.
+ * Copyright (c) 2022 Meta Platforms, Inc. and affiliates.
+ * Copyright (c) 2022 Tejun Heo <tj@kernel.org>
+ * Copyright (c) 2022 David Vernet <dvernet@meta.com>
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <signal.h>
+#include <libgen.h>
+#include <bpf/bpf.h>
+#include <scx/common.h>
+#include "scx_jellybean.bpf.skel.h"
+
+const char help_fmt[] =
+"A jellybean sched_ext scheduler.\n"
+"\n"
+"See the top-level comment in .bpf.c for more details.\n"
+"\n"
+"Usage: %s [-f] [-v]\n"
+"\n"
+"  -f            Use FIFO scheduling instead of weighted vtime scheduling\n"
+"  -v            Print libbpf debug messages\n"
+"  -h            Display this help and exit\n";
+
+#define MEMORY_THRESHOLD 10000.0
+
+static bool verbose;
+static volatile int exit_req;
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+	if (level == LIBBPF_DEBUG && !verbose)
+		return 0;
+	return vfprintf(stderr, format, args);
+}
+
+static void sigint_handler(int simple)
+{
+	exit_req = 1;
+}
+
+static void read_stats(struct scx_jellybean *skel, __u64 *stats)
+{
+	int nr_cpus = libbpf_num_possible_cpus();
+	__u64 cnts[4][nr_cpus];
+	__u32 idx;
+
+	memset(stats, 0, sizeof(stats[0]) * 4);
+
+	for (idx = 0; idx < 4; idx++) {
+		int ret, cpu;
+
+		ret = bpf_map_lookup_elem(bpf_map__fd(skel->maps.stats),
+					  &idx, cnts[idx]);
+		if (ret < 0)
+			continue;
+		for (cpu = 0; cpu < nr_cpus; cpu++)
+			stats[idx] += cnts[idx][cpu];
+	}
+}
+
+double get_memory_from_csv(const char *path, ssize_t *last_byte) {
+	double result = 0;
+	FILE *file;
+	if((file = fopen(path, "r")) != NULL && fseek(file, *last_byte, SEEK_SET) == 0){
+		size_t line_size = 0;
+		char *line = NULL;
+		ssize_t bytes_read = -1;
+		int new_lines = 0;
+		while((bytes_read = getline(&line, &line_size, file)) != -1){
+			*last_byte += bytes_read;
+			new_lines++;
+		}
+		if(new_lines){
+			char *token;
+			char *saveptr;
+			token = strtok_r(line, ",", &saveptr);
+			while (token != NULL) {
+				result = strtod(token, NULL);
+				if (errno == ERANGE) {
+					result = 0;
+					break;
+				}
+				token = strtok_r(NULL, ",", &saveptr);
+			}
+			free(line);
+		}
+		fclose(file);
+	}
+    return result;
+}
+
+
+int main(int argc, char **argv)
+{
+	struct scx_jellybean *skel;
+	struct bpf_link *link;
+	__u32 opt;
+	__u64 ecode;
+
+	libbpf_set_print(libbpf_print_fn);
+	signal(SIGINT, sigint_handler);
+	signal(SIGTERM, sigint_handler);
+restart:
+	skel = SCX_OPS_OPEN(jellybean_ops, scx_jellybean);
+	skel->bss->throttled = false;
+	while ((opt = getopt(argc, argv, "fvh")) != -1) {
+		switch (opt) {
+		case 'f':
+			skel->rodata->fifo_sched = true;
+			break;
+		case 'v':
+			verbose = true;
+			break;
+		default:
+			fprintf(stderr, help_fmt, basename(argv[0]));
+			return opt != 'h';
+		}
+	}
+
+	SCX_OPS_LOAD(skel, jellybean_ops, scx_jellybean, uei);
+	link = SCX_OPS_ATTACH(skel, jellybean_ops, scx_jellybean);
+	
+	ssize_t last_byte = 0;
+	double memory = 0;
+	bool throttled = false;
+	ssize_t history = 0;
+
+	for (__u64 i=0; !exit_req && !UEI_EXITED(skel, uei); i++) {
+		if(i%20==0){
+			__u64 stats[4];
+			read_stats(skel, stats);
+			printf("local=%llu global=%llu batches=%llu memory=%lf throttled=%s\n", stats[0], stats[1], stats[2], memory, skel->bss->throttled ? "true" : "false");
+			fflush(stdout);
+		}
+		memory = get_memory_from_csv("/tmp/memory.log", &last_byte);
+		if(memory > MEMORY_THRESHOLD){
+			history = 40;
+			skel->bss->throttled = throttled = true;
+		}else if(throttled){
+			if(history) history--;
+			else skel->bss->throttled = throttled = false;
+		}
+		usleep(50000);
+	}
+
+	bpf_link__destroy(link);
+	ecode = UEI_REPORT(skel, uei);
+	scx_jellybean__destroy(skel);
+
+	if (UEI_ECODE_RESTART(ecode))
+		goto restart;
+	return 0;
+}
